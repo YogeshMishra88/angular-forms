ebda93e76fe1b542d34d89feaa2a5d0fd6d1a38f
Yogesh Mishra <yogesh.mishra88@gmail.com>
2020-06-05 09:32:37 +0530

    Implemented Reactive Forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
+import {ReactiveFormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,7 +21,8 @@ import { PagenotfoundComponent } from './component/pagenotfound/pagenotfound.com
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/component/reactiveforms/reactiveforms.component.ts
+++ src/app/component/reactiveforms/reactiveforms.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormGroup,FormControl,Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-reactiveforms',
@@ -10,6 +11,27 @@ export class ReactiveformsComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  userForm = new FormGroup({
+    name : new FormControl('Trisha',[Validators.required,Validators.minLength(4),Validators.maxLength(10)]),
+    email: new FormControl(),
+    address : new FormGroup({
+      street : new FormControl(),
+      city : new FormControl(),
+      zipcode : new FormControl(null,[Validators.required,Validators.pattern('^[0-9][1-9]{4}$')])
+    })
+  })
+
+  submitFormValue() : void {
+    console.log(this.userForm.value);
+
+    let data = `Name : ${this.userForm.value.name}, 
+                Email : ${this.userForm.value.name},
+                City : ${this.userForm.value.address.city},
+                Street : ${this.userForm.value.address.street},
+                Zipcode : ${this.userForm.value.address.zipcode}`;
+                alert(data);
   }
 
 }
